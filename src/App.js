0180bbbaccc81749c11dd6a72002410c8f291625
Christian Hubbard <chrishubbard92@hotmail.com>
2020-08-08 21:13:38 -0700

    Building basic form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar'
-
+import Form from './Form'
 
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Form />
+    </>
   );
 }
 
