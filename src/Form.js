2ddad6ddd2b8f672b9be508d4f2b77899cb28ca4
Christian Hubbard <chrishubbard92@hotmail.com>
2020-08-08 21:28:31 -0700

    Building language input and sign-in inputs

--- src/Form.js
+++ src/Form.js
@@ -6,7 +6,7 @@ import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/Avatar";
+import InputLabel from "@material-ui/core/InputLabel";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -25,6 +25,34 @@ class Form extends Component {
                     <Avatar className={classes.avatar}>
                         <LockOutlinedIcon />
                     </Avatar>
+                    <Typography variant='h5'>
+                        Sign In
+                    </Typography>
+                    <Select value='English'>
+                        <MenuItem value='English'>English</MenuItem>
+                        <MenuItem value='French'>French</MenuItem>
+                        <MenuItem value='Spanish'>Spanish</MenuItem>
+                    </Select>
+                    <form className={classes.form}>
+                        <FormControl margin='normal' required fullWidth>
+                            <InputLabel htmlFor='email'>Email</InputLabel>
+                            <Input id='email' name='email' autoFocus />
+                        </FormControl>
+                        <FormControl margin='normal' required fullWidth>
+                            <InputLabel htmlFor='password'>Password</InputLabel>
+                            <Input id='password' name='password' autoFocus />
+                        </FormControl>
+                        <FormControlLabel control={<Checkbox color='primary' />} label='Remember Me'/>
+                        <Button 
+                            variant='contained' 
+                            type='submit' 
+                            fullWidth 
+                            color='primary' 
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                    </form>
                 </Paper>
             </main>
         )
