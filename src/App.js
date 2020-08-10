fee908a023e826cb285c1aa7061b34a632ea8f48
Christian Hubbard <chrishubbard92@hotmail.com>
2020-08-09 21:25:56 -0700

    Building initial Context

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Navbar from './Navbar'
 import Form from './Form'
 import PageContent from './PageContent'
+import {ThemeProvider} from './contexts/ThemeContext'
 
 
 function App() {
   return (
+    <ThemeProvider>
     <PageContent>
       <Navbar />
       <Form />
     </ PageContent >
+    </ThemeProvider>  
   );
 }
 
